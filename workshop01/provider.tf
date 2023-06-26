e193ec6e67d412bb5474888554f1177bd02fbdaf
FT Tan <fangtiannn@gmail.com>
2023-06-26 16:54:24 +0800

    edit provider source

--- workshop01/provider.tf
+++ workshop01/provider.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         # default values
         docker = {
-            source = "kreyzweker/docker"
+            source = "kreuzwerker/docker"
             version = "3.0.2"
         }
         digitalocean = {
