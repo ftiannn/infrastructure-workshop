3985100d42aa11a88693843b832eee137aede74a
FT Tan <fangtiannn@gmail.com>
2023-06-28 16:02:32 +0800

    added inventory

--- workshop03/deploy/provider.tf
+++ workshop03/deploy/provider.tf
@@ -1,19 +1,18 @@
 terraform {
-    required_providers {
-        digitalocean = {
-            source = "digitalocean/digitalocean"
-            version = "2.26.0"
-        }
-        local = {
-            source = "hashicorp/local"
-            version = "2.4.0"
-        }
+  required_version = ">1.0.0"
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
+
+     local = {
+      source = "hashicorp/local"
+      version = "2.1.0"
+    }
+  }
 }
-
-provider digitalocean {
-    token = var.do_token
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
 }
-
-
-provider local { }
